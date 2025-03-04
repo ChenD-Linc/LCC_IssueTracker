13f4ae51c315137528af5ccb0e32fddb3a753228
Chen D <chen.ding@lincolnuni.ac.nz>
2025-03-04 12:00:52 +0800

    Update connect.py

--- issuetracker/connect.py
+++ issuetracker/connect.py
@@ -1,5 +1,5 @@
 dbuser = 'root'  # Change to your MySQL username
-dbpass = 'smartapps23'  # Change to your MySQL password
+dbpass = 'smartapps123'  # Change to your MySQL password
 dbhost = 'localhost'
 dbport = '3306'
 dbname = 'lcc_issue_tracker'
